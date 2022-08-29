015cd61f4af96cf38fb4a6ba6bae05a8ec36f52a
LeeTaehyung <thlee@kb-sys.com>
2022-08-29 11:08:04 +0900

    Initial Commit

---
+++ ScrollReaderTest/ScrollReaderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollReaderTestApp.swift
+//  ScrollReaderTest
+//
+//  Created by Taehyung Lee on 2022/08/29.
+//
+
+import SwiftUI
+
+@main
+struct ScrollReaderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
